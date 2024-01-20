c643ac581c88f0ca52f47f5e1a51718c0c381ecb
geek-roy <sahaankit0701@gmail.com>
2024-01-20 15:31:51 +0530

    Student details page complete ui

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ class Face_Recognition_System:
         self.root.geometry("1530x790+0+0")
         self.root.title("face recognition system")
 
-        #first image
-        img=Image.open("./college_images/Stanford.jpg")
+         #first image
+        img=Image.open("Attendance_Management_System/college_images/Stanford.jpg")
         img=img.resize((500,130),Image.LANCZOS)
         self.photoimg=ImageTk.PhotoImage(img)
 
@@ -18,7 +18,7 @@ class Face_Recognition_System:
         f_lbl.place(x=0,y=0,width=500,height=130)
 
         #second image
-        img1=Image.open("./college_images/facialrecognition.png")
+        img1=Image.open("Attendance_Management_System/college_images/facialrecognition.png")
         img1=img1.resize((500,130),Image.LANCZOS)
         self.photoimg1=ImageTk.PhotoImage(img1)
 
@@ -26,7 +26,7 @@ class Face_Recognition_System:
         f_lbl.place(x=520,y=0,width=500,height=130)
 
         #third image
-        img2=Image.open("./college_images/u.jpg")
+        img2=Image.open("Attendance_Management_System/college_images/u.jpg")
         img2=img2.resize((500,130),Image.LANCZOS)
         self.photoimg2=ImageTk.PhotoImage(img2)
 
@@ -35,7 +35,7 @@ class Face_Recognition_System:
 
 
         #background image
-        img3=Image.open("./college_images/bg1.jpg")
+        img3=Image.open("Attendance_Management_System/college_images/bg1.jpg")
         img3=img3.resize((1530,710),Image.LANCZOS)
         self.photoimg3=ImageTk.PhotoImage(img3)
 
@@ -47,7 +47,7 @@ class Face_Recognition_System:
         title_lbl.place(x=0,y=0,width=1530,height=45)
 
         #student button
-        img4=Image.open("./college_images/student.jpg")
+        img4=Image.open("Attendance_Management_System/college_images/student.jpg")
         img4=img4.resize((220,220),Image.LANCZOS)
         self.photoimg4=ImageTk.PhotoImage(img4)
 
@@ -58,7 +58,7 @@ class Face_Recognition_System:
         b1_1.place(x=200,y=300,width=220,height=40)
 
         #Detect face button
-        img5=Image.open("./college_images/face_detector1.jpg")
+        img5=Image.open("Attendance_Management_System/college_images/face_detector1.jpg")
         img5=img5.resize((220,220),Image.LANCZOS)
         self.photoimg5=ImageTk.PhotoImage(img5)
 
@@ -69,7 +69,7 @@ class Face_Recognition_System:
         b1_1.place(x=500,y=300,width=220,height=40)
 
         #Attendance face button
-        img6=Image.open("./college_images/attendance.jpg")
+        img6=Image.open("Attendance_Management_System/college_images/attendance.jpg")
         img6=img6.resize((220,220),Image.LANCZOS)
         self.photoimg6=ImageTk.PhotoImage(img6)
 
@@ -80,7 +80,7 @@ class Face_Recognition_System:
         b1_1.place(x=800,y=300,width=220,height=40)
 
         #Help button
-        img7=Image.open("./college_images/help.jpg")
+        img7=Image.open("Attendance_Management_System/college_images/help.jpg")
         img7=img7.resize((220,220),Image.LANCZOS)
         self.photoimg7=ImageTk.PhotoImage(img7)
 
@@ -91,7 +91,7 @@ class Face_Recognition_System:
         b1_1.place(x=1100,y=300,width=220,height=40)
 
         #Train button
-        img8=Image.open("./college_images/Train.jpg")
+        img8=Image.open("Attendance_Management_System/college_images/Train.jpg")
         img8=img8.resize((220,220),Image.LANCZOS)
         self.photoimg8=ImageTk.PhotoImage(img8)
 
@@ -102,7 +102,7 @@ class Face_Recognition_System:
         b1_1.place(x=200,y=580,width=220,height=40)
 
         #Photos face button
-        img9=Image.open("./college_images/opencv_face_reco_more_data.jpg")
+        img9=Image.open("Attendance_Management_System/college_images/opencv_face_reco_more_data.jpg")
         img9=img9.resize((220,220),Image.LANCZOS)
         self.photoimg9=ImageTk.PhotoImage(img9)
 
@@ -113,7 +113,7 @@ class Face_Recognition_System:
         b1_1.place(x=500,y=580,width=220,height=40)
 
         #Developer face button
-        img10=Image.open("./college_images/Team-Management-Software-Development.jpg")
+        img10=Image.open("Attendance_Management_System/college_images/Team-Management-Software-Development.jpg")
         img10=img10.resize((220,220),Image.LANCZOS)
         self.photoimg10=ImageTk.PhotoImage(img10)
 
@@ -124,7 +124,7 @@ class Face_Recognition_System:
         b1_1.place(x=800,y=580,width=220,height=40)
 
         #Exit face button
-        img11=Image.open("./college_images/exit.jpg")
+        img11=Image.open("Attendance_Management_System/college_images/exit.jpg")
         img11=img11.resize((220,220),Image.LANCZOS)
         self.photoimg11=ImageTk.PhotoImage(img11)
 
--- student.py
+++ student.py
@@ -10,7 +10,7 @@ class Student:
         self.root.title("face recognition system")
        
         #first image
-        img=Image.open("./college_images/face-recognition.png")
+        img=Image.open("Attendance_Management_System/college_images/facialrecognition.png")
         img=img.resize((500,130),Image.LANCZOS)
         self.photoimg=ImageTk.PhotoImage(img)
 
@@ -18,23 +18,23 @@ class Student:
         f_lbl.place(x=0,y=0,width=500,height=130)
 
         #second image
-        img1=Image.open("./college_images/facialrecognition.png")
+        img1=Image.open("Attendance_Management_System/college_images/smart-attendance.jpg")
         img1=img1.resize((500,130),Image.LANCZOS)
         self.photoimg1=ImageTk.PhotoImage(img1)
 
         f_lbl=Label(self.root,image=self.photoimg1)
-        f_lbl.place(x=520,y=0,width=500,height=130)
+        f_lbl.place(x=500,y=0,width=500,height=130)
 
         #third image
-        img2=Image.open("./college_images/iStock-182059956_18390_t12.jpg")
-        img2=img2.resize((500,130),Image.LANCZOS)
+        img2=Image.open("Attendance_Management_System/college_images/university.jpg")
+        img2=img2.resize((550,130),Image.LANCZOS)
         self.photoimg2=ImageTk.PhotoImage(img2)
 
         f_lbl=Label(self.root,image=self.photoimg2)
-        f_lbl.place(x=1040,y=0,width=500,height=130)
+        f_lbl.place(x=1000,y=0,width=550,height=130)
 
         #background image
-        img3=Image.open("./college_images/bg1.jpg")
+        img3=Image.open("Attendance_Management_System/college_images/bg1.jpg")
         img3=img3.resize((1530,710),Image.LANCZOS)
         self.photoimg3=ImageTk.PhotoImage(img3)
 
@@ -42,59 +42,252 @@ class Student:
         bg_img.place(x=0,y=130,width=1530,height=710)
 
         #title
-        title_lbl=Label(bg_img,text="STUDENT MANAGEMENT SYSTEM",font=("times new roman",35,"bold"),bg="white",fg="dark blue")
+        title_lbl=Label(bg_img,text="STUDENT MANAGEMENT SYSTEM",font=("times new roman",35,"bold"),bg="white",fg="red")
         title_lbl.place(x=0,y=0,width=1530,height=45)
 
         main_frame=Frame(bg_img,bd=2,bg="white")
-        main_frame.place(x=5,y=55,width=1500,height=600)
-        #main_frame.place(x=20,y=55,width=1480,height=600)
-
-        #left label frame 
-         
-        Left_frame=LabelFrame(main_frame,bd=2,bg="white",relief=RIDGE,text="Student Details",font=("times new roman",12,"bold"))
-        Left_frame.place(x=10,y=10,width=650,height=580)
-        #Left_frame.place(x=10,y=10,width=760,height=580)
+        main_frame.place(x=20,y=50,width=1480,height=600)
         
-
-        img_left=Image.open("./college_images/AdobeStock_303989091.jpeg")
-        img_left=img_left.resize((640,130),Image.LANCZOS)
-        #img_left=img_left.resize((720,130),Image.LANCZOS)
+        #left label frame  
+        Left_frame=LabelFrame(main_frame,bd=2,bg="white",relief=RIDGE,text="Student Details",font=("times new roman",12,"bold"))
+        Left_frame.place(x=5,y=10,width=734,height=580)
+        
+        img_left=Image.open("Attendance_Management_System/college_images/AdobeStock_303989091.jpeg")
+        img_left=img_left.resize((720,130),Image.LANCZOS)
         self.photoimg_left=ImageTk.PhotoImage(img_left)
-
-        f_lbl=Label(self.root,image=self.photoimg_left)
-        f_lbl.place(x=35,y=220,width=620,height=130)
+        
+        f_lbl=Label(Left_frame,image=self.photoimg_left)
+        f_lbl.place(x=5,y=0,width=720,height=130)
       
         #current course
         Current_course_frame=LabelFrame(Left_frame,bd=2,bg="white",relief=RIDGE,text="Current course  information",font=("times new roman",12,"bold"))
-        Current_course_frame.place(x=15,y=130,width=620,height=130)
-        #Current_course_frame.place(x=10,y=10,width=760,height=580)
+        Current_course_frame.place(x=5,y=135,width=720,height=150)
+        
 
         #DEPARTMENT
-        dep_label=Label(Current_course_frame,text="Department",font=("times new roman",12,"bold"),bg="white")
+        dep_label=Label(Current_course_frame,text="Department",font=("times new roman",13,"bold"),bg="white")
         dep_label.grid(row=0,column=0,sticky=W)
-
-        dep_combo=ttk.Combobox(Current_course_frame,font=("times new roman",12,"bold"),width=17,state="read only")
+        dep_combo=ttk.Combobox(Current_course_frame,font=("times new roman",13,"bold"),width=20,state="read only")
         dep_combo["values"]=("Select Department","Computer","IT","Civil","Mechanical","Electrical")
         dep_combo.current(0)
         dep_combo.grid(row=0,column=1,padx=2,pady=2,sticky=W)
-
+         
         #COURSE
-        course_label=Label(Current_course_frame,text="Course",font=("times new roman",12,"bold"),bg="white")
-        course_label_label.grid(row=0,column=2,sticky=W)
+        course_label=Label(Current_course_frame,text="Course",font=("times new roman",13,"bold"),bg="white")
+        course_label.grid(row=0,column=2,sticky=W)
 
-        course_combo=ttk.Combobox(Current_course_frame,font=("times new roman",12,"bold"),width=17,state="read only")
+        course_combo=ttk.Combobox(Current_course_frame,font=("times new roman",13,"bold"),width=20,state="read only")
         course_combo["values"]=("Select Course","EE","SE","TE","BE")
         course_combo.current(0)
-        course_combo.grid(row=0,column=1,padx=2,pady=2,sticky=W)
+        course_combo.grid(row=0,column=3,padx=2,pady=10,sticky=W)
+        
+        #Year
+        year_label=Label(Current_course_frame,text="Year",font=("times new roman",13,"bold"),bg="white")
+        year_label.grid(row=1,column=0,sticky=W)
 
+        year_combo=ttk.Combobox(Current_course_frame,font=("times new roman",13,"bold"),width=20,state="read only")
+        year_combo["values"]=("Select Year","2020-21","2021-22","2022-23","2023-24")
+        year_combo.current(0)
+        year_combo.grid(row=1,column=1,padx=2,pady=10,sticky=W)
 
-        #Right label frame 
-         
+        #Semester
+        semester_label=Label(Current_course_frame,text="Semester",font=("times new roman",13,"bold"),bg="white")
+        semester_label.grid(row=1,column=2,sticky=W)
+
+        semester_combo=ttk.Combobox(Current_course_frame,font=("times new roman",13,"bold"),width=20,state="read only")
+        semester_combo["values"]=("Select Semester","Odd Semester","Even Semester")
+        semester_combo.current(0)
+        semester_combo.grid(row=1,column=3,padx=2,pady=10,sticky=W)
+        
+        #class student information
+        Class_Student_frame=LabelFrame(Left_frame,bd=2,bg="white",relief=RIDGE,text="Class Student information",font=("times new roman",12,"bold"))
+        Class_Student_frame.place(x=5,y=250,width=720,height=300)
+
+        #Student Id
+        studentId_label=Label(Class_Student_frame,text="StudentID",font=("times new roman",13,"bold"),bg="white")
+        studentId_label.grid(row=0,column=0,padx=10,pady=5,sticky=W)
+
+        studentID_entry=ttk.Entry(Class_Student_frame,width=20,font=("times new roman",13,"bold"))
+        studentID_entry.grid(row=0,column=1,padx=10,pady=5,sticky=W)
+
+        #Student name
+        studentName_label=Label(Class_Student_frame,text="Student Name",font=("times new roman",13,"bold"),bg="white")
+        studentName_label.grid(row=0,column=2,padx=10,pady=5,sticky=W)
+
+        studentName_entry=ttk.Entry(Class_Student_frame,width=20,font=("times new roman",13,"bold"))
+        studentName_entry.grid(row=0,column=3,padx=10,pady=5,sticky=W)
+
+        #class didvision
+        class_div_label=Label(Class_Student_frame,text="Class Division",font=("times new roman",13,"bold"),bg="white")
+        class_div_label.grid(row=1,column=0,padx=10,pady=5,sticky=W)
+
+        class_div_entry=ttk.Entry(Class_Student_frame,width=20,font=("times new roman",13,"bold"))
+        class_div_entry.grid(row=1,column=1,padx=10,pady=5,sticky=W)
+
+        #roll
+        roll_label=Label(Class_Student_frame,text="Roll No",font=("times new roman",13,"bold"),bg="white")
+        roll_label.grid(row=1,column=2,padx=10,pady=5,sticky=W)
+
+        roll_entry=ttk.Entry(Class_Student_frame,width=20,font=("times new roman",13,"bold"))
+        roll_entry.grid(row=1,column=3,padx=10,pady=5,sticky=W)
+
+        #gender
+        gender_label=Label(Class_Student_frame,text="Gender",font=("times new roman",13,"bold"),bg="white")
+        gender_label.grid(row=2,column=0,padx=10,pady=5,sticky=W)
+
+        gender_entry=ttk.Entry(Class_Student_frame,width=20,font=("times new roman",13,"bold"))
+        gender_entry.grid(row=2,column=1,padx=10,pady=5,sticky=W)
+
+        #dob
+        dob_label=Label(Class_Student_frame,text="DOB",font=("times new roman",13,"bold"),bg="white")
+        dob_label.grid(row=2,column=2,padx=10,pady=5,sticky=W)
+
+        dob_entry=ttk.Entry(Class_Student_frame,width=20,font=("times new roman",13,"bold"))
+        dob_entry.grid(row=2,column=3,padx=10,pady=5,sticky=W)
+
+        #email
+        email_label=Label(Class_Student_frame,text="Email",font=("times new roman",13,"bold"),bg="white")
+        email_label.grid(row=3,column=0,padx=10,pady=5,sticky=W)
+
+        email_entry=ttk.Entry(Class_Student_frame,width=20,font=("times new roman",13,"bold"))
+        email_entry.grid(row=3,column=1,padx=10,pady=5,sticky=W)
+
+        #phoneno
+        phoneno_label=Label(Class_Student_frame,text="Phone No",font=("times new roman",13,"bold"),bg="white")
+        phoneno_label.grid(row=3,column=2,padx=10,pady=5,sticky=W)
+
+        phoneno_entry=ttk.Entry(Class_Student_frame,width=20,font=("times new roman",13,"bold"))
+        phoneno_entry.grid(row=3,column=3,padx=10,pady=5,sticky=W)
+
+        #address
+        address_label=Label(Class_Student_frame,text="Address",font=("times new roman",13,"bold"),bg="white")
+        address_label.grid(row=4,column=0,padx=10,pady=5,sticky=W)
+
+        address_entry=ttk.Entry(Class_Student_frame,width=20,font=("times new roman",13,"bold"))
+        address_entry.grid(row=4,column=1,padx=10,pady=5,sticky=W)
+
+        #teacher name
+        teacher_label=Label(Class_Student_frame,text="Teacher Name",font=("times new roman",13,"bold"),bg="white")
+        teacher_label.grid(row=4,column=2,padx=10,pady=5,sticky=W)
+
+        teacher_entry=ttk.Entry(Class_Student_frame,width=20,font=("times new roman",13,"bold"))
+        teacher_entry.grid(row=4,column=3,padx=10,pady=5,sticky=W)
+
+        #radio buttons
+        radiobtn1 = ttk.Radiobutton(Class_Student_frame,text="Take Photo Sample",value="Yes")
+        radiobtn1.grid(row=6,column=0)
+
+        radiobtn2 = ttk.Radiobutton(Class_Student_frame,text="No Photo Sample",value="Yes")
+        radiobtn2.grid(row=6,column=1)
+
+        #buttons frame
+        btn_frame=Frame(Class_Student_frame,bd=2,relief=RIDGE,bg="white")
+        btn_frame.place(x=0,y=200,width=715,height=35)
+        
+        save_btn=Button(btn_frame,text="Save",width=17,font=("times new roman",13,"bold"),bg="blue",fg="white")
+        save_btn.grid(row=0,column=0)
+
+        update_btn=Button(btn_frame,text="Update",width=17,font=("times new roman",13,"bold"),bg="blue",fg="white")
+        update_btn.grid(row=0,column=1)
+
+        delete_btn=Button(btn_frame,text="Delete",width=17,font=("times new roman",13,"bold"),bg="blue",fg="white")
+        delete_btn.grid(row=0,column=2)
+
+        reset_btn=Button(btn_frame,text="Reset",width=17,font=("times new roman",13,"bold"),bg="blue",fg="white")
+        reset_btn.grid(row=0,column=3)
+
+         #buttons frame
+        btn_frame1=Frame(Class_Student_frame,bd=2,relief=RIDGE,bg="white")
+        btn_frame1.place(x=0,y=235,width=715,height=35)
+
+        take_photo_btn=Button(btn_frame1,text="Take Photo Sample",width=35,font=("times new roman",13,"bold"),bg="blue",fg="white")
+        take_photo_btn.grid(row=0,column=0)
+
+        update_photo_btn=Button(btn_frame1,text="Update Photo Sample",width=35,font=("times new roman",13,"bold"),bg="blue",fg="white")
+        update_photo_btn.grid(row=0,column=1)
+
+        #Right label frame  
         Right_frame=LabelFrame(main_frame,bd=2,bg="white",relief=RIDGE,text="Student Details",font=("times new roman",12,"bold"))
-        Right_frame.place(x=670,y=10,width=660,height=580)
-        #Right_frame.place(x=78,y=10,width=660,height=580)
+        Right_frame.place(x=745,y=10,width=723,height=580)
 
+        img_right=Image.open("Attendance_Management_System/college_images/Student.jpg")
+        img_right=img_right.resize((710,130),Image.LANCZOS)
+        self.photoimg_right=ImageTk.PhotoImage(img_right)
+        
+        f_lbl=Label(Right_frame,image=self.photoimg_right)
+        f_lbl.place(x=5,y=0,width=710,height=130)
 
+        #####Search System
+        Search_frame=LabelFrame(Right_frame,bd=2,bg="white",relief=RIDGE,text="Search System",font=("times new roman",12,"bold"))
+        Search_frame.place(x=5,y=135,width=710,height=70 )
+
+        search_label=Label(Search_frame,text="Search By:",font=("times new roman",13,"bold"),bg="red",fg="white")
+        search_label.grid(row=0,column=0,padx=10,pady=5,sticky=W)
+
+        search_combo=ttk.Combobox(Search_frame,font=("times new roman",13,"bold"),width=15,state="read only")
+        search_combo["values"]=("Select","Roll_No","Phone_No")
+        search_combo.current(0)
+        search_combo.grid(row=0,column=1,padx=2,pady=10,sticky=W)
+
+        search_entry=ttk.Entry(Search_frame,width=15,font=("times new roman",13,"bold"))
+        search_entry.grid(row=0,column=2,padx=10,pady=5,sticky=W)
+
+        #button
+        search_btn=Button(Search_frame,text="Search",width=11,font=("times new roman",12,"bold"),bg="blue",fg="white")
+        search_btn.grid(row=0,column=3,padx=4)
+
+        showAll_btn=Button(Search_frame,text="ShowAll",width=11,font=("times new roman",12,"bold"),bg="blue",fg="white")
+        showAll_btn.grid(row=0,column=4)
+
+        ##Table Frame
+        table_frame=Frame(Right_frame,bd=2,bg="white",relief=RIDGE)
+        table_frame.place(x=5,y=210,width=710,height=350 )
+
+        Scroll_x=ttk.Scrollbar(table_frame,orient=HORIZONTAL)
+        Scroll_y=ttk.Scrollbar(table_frame,orient=VERTICAL)
+
+        self.student_table=ttk.Treeview(table_frame,column=("dep","course","year","sem","id","name","div","roll","gender","dob","email","phone","address","teacher","photo"),xscrollcommand=Scroll_x.set,yscrollcommand=Scroll_y.set)
+
+        Scroll_x.pack(side=BOTTOM,fill=X)
+        Scroll_y.pack(side=RIGHT,fill=Y)
+        Scroll_x.config(command=self.student_table.xview)
+        Scroll_y.config(command=self.student_table.xview)
+
+        self.student_table.heading("dep",text="Department")
+        self.student_table.heading("course",text="Course")
+        self.student_table.heading("year",text="Year")
+        self.student_table.heading("sem",text="Semester")
+        self.student_table.heading("id",text="Student Id")
+        self.student_table.heading("name",text="Name")
+        self.student_table.heading("div",text="Division")
+        self.student_table.heading("roll",text="Roll_No")
+        self.student_table.heading("gender",text="Gender")
+        self.student_table.heading("dob",text="DOB")
+        self.student_table.heading("email",text="Email")
+        self.student_table.heading("phone",text="Phone")
+        self.student_table.heading("address",text="Address")
+        self.student_table.heading("teacher",text="Teacher")
+        self.student_table.heading("photo",text="Photo Sample Status")
+        self.student_table["show"]="headings"
+
+        self.student_table.column("dep",width=100)
+        self.student_table.column("course",width=100)
+        self.student_table.column("year",width=100)
+        self.student_table.column("sem",width=100)
+        self.student_table.column("id",width=100)
+        self.student_table.column("name",width=100)
+        self.student_table.column("div",width=100)
+        self.student_table.column("roll",width=100)
+        self.student_table.column("gender",width=100)
+        self.student_table.column("dob",width=100)
+        self.student_table.column("email",width=100)
+        self.student_table.column("phone",width=100)
+        self.student_table.column("address",width=100)
+        self.student_table.column("teacher",width=100)
+        self.student_table.column("photo",width=150)
+        self.student_table["show"]="headings"
+        
+        self.student_table.pack(fill=BOTH,expand=1)
 
 
 
